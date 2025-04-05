6d8c21166215e465f3ac19b57df4a918762c569b
nalmatov <nalmatov6@gmail.com>
2025-04-06 00:38:24 +0600

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: 3000,
+    strictPort: true,
+    allowedHosts: [
+      'ltc-rates.com',
+      'www.ltc-rates.com',
+    ],
+  },
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
+});
